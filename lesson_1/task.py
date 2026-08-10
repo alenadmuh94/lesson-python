444f172f97a670fc52741392ea53fff27b2d72f5
tensor-corp\av.dmuhovskaya <av.dmuhovskaya@tensor.ru>
2026-08-10 08:41:36 +0300

    add lesson_1

---
+++ lesson_1/task.py
@@ -0,0 +1,9 @@
+def hello_world():
+    """Привет, мир!"""
+
+    # todo Здесь нужно написать код
+
+    hello = 'Hello'
+    world = 'world'
+    hello_and_world = hello + ' ' + world + "!"
+    return hello_and_world
